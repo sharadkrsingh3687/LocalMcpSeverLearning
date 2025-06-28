982b1cfb2c47a35cddb9c25b8e49140a650cbb98
Sharad Kumar Singh <sharadkrsingh3687@gmail.com>
2025-06-28 10:56:43 +0000

    correctioning the data

--- src/sample.mcpserver/sample.mcpserver.local/Prompts/SamplePrompts.cs
+++ src/sample.mcpserver/sample.mcpserver.local/Prompts/SamplePrompts.cs
@@ -4,13 +4,13 @@ using System.ComponentModel;
 [McpServerPromptType]
 public static class SamplePrompts
 {
-    [McpServerPrompt(nameof = "factorial_prompt"), Description("Calculate the factorial of a number.")]
+    [McpServerPrompt(Name = "factorial_prompt"), Description("Calculate the factorial of a number.")]
     public static string FactorialPrompt(int number)
     {
         return $"Calculate the factorial of {number}.";
     }
 
-    [McpServerPrompt(nameof = "find_primenumber_prompt"), Description("Find the Prime number between two numbers.")]
+    [McpServerPrompt(Name = "find_primenumber_prompt"), Description("Find the Prime number between two numbers.")]
     public static string FindPrimeNumberPrompt(long start, long end)
     {
         return $"Find all prime numbers between {start} and {end}.";
